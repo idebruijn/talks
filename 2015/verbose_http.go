0f2c3d0552adf5d55b3547e3d8dff79b4bef8793
Ernest Micklei <ernest.micklei@gmail.com>
2015-08-26 17:54:27 +0200

    code + forest plaatje
    Change-Id: I1483d8f3288c3ad055d35f66281e7bfe3deb2cf9

---
+++ 2015/verbose_http.go
@@ -0,0 +1,23 @@
+client := new(http.Client)
+payload := strings.NewReader("key=value")
+req , err := http.NewRequest("POST",
+	"http://localhost:12346/v1/properties",
+	payload)
+if err != nil {
+	log.Fatal("invalid url:%v",err)
+}
+req.Header.Add("Content-Type","text/plain")
+req.Header.Add("Accept","application/json")
+resp, err := client.Do(req)
+if err != nil {
+	log.Fatal("failed to send request:%v",err)
+}
+defer resp.Body.Close()
+body, err := ioutil.ReadAll(resp.Body)
+if err != nil {
+	log.Fatal("failed to read response:%v",err)
+}
+if len(body) == 0 { return }
+var serviceError ServiceError
+err = json.Unmarshal(body, &serviceError )
+...
